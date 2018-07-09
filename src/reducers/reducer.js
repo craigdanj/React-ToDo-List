850bfd700ff97df0f9313f8f9eae4e6c501a6537
Craig <craigdanj@gmail.com>
2018-07-09 11:17:51 +0530

    cleanup

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -28,26 +28,18 @@ const reducer = (state = initialState, action) => {
 	switch(action.type) {
 
 		case "ADD_TODO": 
-			console.log(action);
 			var newState = {...state};
 			newState.items = [...newState.items, {text: action.payload.text, checked: false}];
-			console.log(newState);
 
 			return newState;
 
 		case "REMOVE_TODO":
 
-			console.log(action.payload.index);
-
 			var newState = {...state};
-			// var newState = Object.assign({}, state);
-			// newState.items.splice(action.payload.index, 1);
 			newState.items = [
 				...state.items.slice(0, action.payload.index),
 				...state.items.slice(action.payload.index+1)
 			]
-
-			console.log(newState);
 
 			return newState;
 
